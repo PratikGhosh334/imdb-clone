aa4a701d5f178313817ae6f713f6e3c0926d1104
Pratik Ghosh <pratikghosh334@gmail.com>
2024-04-06 16:05:04 +0530

    Created about page

---
+++ imdb-next/src/app/about/page.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4 '>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>Introducing IMDb's About page, your gateway to the ultimate cinematic experience! Since its inception, 
+            IMDb has been the go-to destination for millions of movie enthusiasts worldwide, offering a comprehensive
+            platform to explore the rich tapestry of film and television. With an extensive database comprising millions
+            of titles and celebrities, IMDb is your trusted companion on the journey through the vast landscape of entertainment.
+        </p>
+
+        <p>IMDb has grown into a cultural phenomenon, serving as a beacon for cinephiles, industry professionals, and casual viewers
+        alike. From classic Hollywood classics to cutting-edge indie flicks, from blockbuster franchises to obscure cult favorites, 
+        IMDb's user-friendly interface and robust features provide unparalleled access to the world of cinema. With detailed information, 
+        user-generated ratings, and reviews, IMDb empowers audiences to discover new favorites, revisit timeless classics, and engage in 
+        lively discussions about their most beloved films and TV shows.</p>
+
+        <p>But IMDb is more than just a database – it's a thriving community where fans come together to share their passion, knowledge, 
+        and enthusiasm for all things entertainment. Whether you're a seasoned critic or a casual moviegoer, IMDb offers a platform for dialogue, 
+        discovery, and connection, enriching the collective experience of storytelling in all its forms. Join us as we continue to celebrate the magic 
+        of cinema and television, one click at a time, here on IMDb.</p>
+    </div>
+  )
+}
