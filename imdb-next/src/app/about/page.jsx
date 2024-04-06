3ac74dd25eaac6be8fd0338bc9fea504e7be2c7a
Pratik Ghosh <pratikghosh334@gmail.com>
2024-04-06 22:06:32 +0530

    Created the navbar component

--- imdb-next/src/app/about/page.jsx
+++ imdb-next/src/app/about/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className='max-w-6xl mx-auto p-3 space-y-4 '>
+    <div className='max-w-6xl mx-auto p-3 space-y-4  '>
         <h1 className='text-2xl font-medium text-amber-600'>About</h1>
         <p>Introducing IMDb's About page, your gateway to the ultimate cinematic experience! Since its inception, 
             IMDb has been the go-to destination for millions of movie enthusiasts worldwide, offering a comprehensive
